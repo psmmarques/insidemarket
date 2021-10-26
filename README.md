344779b6deda01fc9c1c2cf3d010fa1c5078051a
psmarqu3s <psmarques@gmail.com>
2021-10-26 17:45:53 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# insidemarket
+## Rodar a aplicação
+
+Lembre-se de alterar o token na core/constant.py assim não haverá problema com o robo
+prod
+
+Para rodar/debugar esta aplicação é necessario instalar o python 3.8 ou superior
+instalar as bibliotecas contidas no requirements.txt
+
+#Arquivo .env
+Crie o arquivo .env dentro da pasta insidemarketb3/.env
+DATABASE_URL=postgres://jzeaeczpzhhbho:172821100e137f85686909b308b0b13313dfcba4237278dfff0d30a19920cb23@ec2-34-194-14-176.compute-1.amazonaws.com:5432/d26bl1b5bbi5ha
+
+
+#instalação das libs
+pip install -f requirements.txt
+
+#rodar o projeto
+python manage.py runserver
+
+ou se quiser publicar no docker
+docker-compose up -d
+
+#remover
+docker-compose down --rmi all
---
+++ core/constant.py
@@ -0,0 +1,22 @@
+NOMEBOT = 'garcom'
+TOKEN=''
+URLMESSAGE = 'https://api.telegram.org/bot{0}/sendMessage'
+URLANIMATION = 'https://api.telegram.org/bot{0}/sendAnimation'
+URLLEAVECHAT = 'https://api.telegram.org/bot{0}/leaveChat'
+URLAJUSTEB3 = 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/Ajustes1.asp'
+URLATIVOB3 = 'https://finance-services.msn.com/Market.svc/ChartAndQuotes?symbols=56.1.{0}.BSP&chartType=1d&isETF=false&iseod=False&lang=pt-BR&isCS=false&isVol=true&prime=true'
+CODIGOMINI = 'www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/Ajustes1.asp'
+URLVARIACAOB3 = 'http://cotacao.b3.com.br/mds/api/v1/InstrumentPriceFluctuation/ibov'
+URLVARIACAOVOLB3 = 'http://cotacao.b3.com.br/mds/api/v1/InstrumentTradeVolume/vista'
+NOTICIASURL = 'https://www.forexfactory.com/calendar.php'
+CALENDARIORSS = 'https://www.myfxbook.com/rss/forex-economic-calendar-events'
+ATIVOURL = 'https://api.iextrading.com/1.0/stock/{0}/batch?types=quote'
+
+FIXERKEY = 'be9ef24171f50b06d23927a43ab804c8'
+FIXERURL = 'http://data.fixer.io/api/'
+
+CRYPTOURL = 'https://min-api.cryptocompare.com/data/price?fsym={0}&tsyms={1}&api={2}'
+CRYPTOURL2 = 'https://min-api.cryptocompare.com/data/generateAvg?fsym={0}&tsym={1}&e=Kraken&api={2}'
+CRYPTOKEY = '9c740dfeb6d1992887c518560ac1cc9fdeb7185cd7f022c24b874160ac4cfb59'
+
+#https://api.telegram.org/bot651533119:AAEYCBPF2VUOO_07Bck_4hgYImitIdAKYtw/setWebhook?url=https://73bc9bad80c2.ngrok.io/event/
